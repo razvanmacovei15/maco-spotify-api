255e6ecaf7c861b2a498f38ebdf985b7dd4e9783
RazvanMacovei15 <razvanmc15@gmail.com>
2025-04-17 19:22:15 +0300

    first commit

--- src/main/java/com/maco/SpotifyClient.java
+++ src/main/java/com/maco/SpotifyClient.java
@@ -1,22 +1,16 @@
 package com.maco;
 
-import com.maco.api.TimePeriod;
-import com.maco.api.annotations.TimeRange;
-import com.maco.api.annotations.ParameterProcessor;
 import com.maco.api.track.TrackService;
 import com.maco.auth.config.SpotifyConfig;
 import com.maco.auth.callbackserver.AuthService;
 import com.maco.auth.token.TokenManager;
 import com.maco.model.Track;
-import com.maco.model.User;
-import com.maco.service.SpotifyService;
+import com.maco.service.AnnotatedServiceInvoker;
 import com.maco.utils.Browser;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.List;
-import java.util.Map;
 
 public class SpotifyClient {
     private final SpotifyConfig clientConfig;
@@ -55,29 +49,6 @@ public class SpotifyClient {
             throw new IllegalStateException("Client is not authenticated. Please call authenticate() first.");
         }
 
-        Method method = null;
-        for (Method m : trackService.getClass().getDeclaredMethods()) {
-            if (m.isAnnotationPresent(TimeRange.class)) {
-                method = m;
-                break;
-            }
-        }
-
-        if (method == null) {
-            throw new IllegalStateException("No method with @TimeRange annotation found");
-        }
-
-        // Create parameters for the method
-        Object[] args = new Object[] {
-            TimePeriod.SHORT_TERM,  // timePeriod
-            50,                     // limit
-            0                       // offset
-        };
-
-        // Process parameters using ParameterProcessor
-        Map<String, String> processedParams = ParameterProcessor.processParameters(method, args);
-
-        // Invoke the method with the processed parameters
-        return (List<Track>) method.invoke(trackService, args);
+        return AnnotatedServiceInvoker.invokeAnnotatedMethod(trackService, List.class);
     }
 }
