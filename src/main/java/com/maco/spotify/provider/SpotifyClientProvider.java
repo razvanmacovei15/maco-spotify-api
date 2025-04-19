b45fd5ae8afacaf27b77b654e748f28babae487a
razvanmacovei15 <razvanmc15@gmail.com>
2025-04-19 21:18:24 +0300

    implementat preluare date

--- src/main/java/com/maco/spotify/provider/SpotifyClientProvider.java
+++
@@ -1,8 +0,0 @@
-package com.maco.spotify.provider;
-
-import com.maco.spotify.api.client.SpotifyClient;
-
-public interface SpotifyClientProvider {
-    SpotifyClient getClient(String userId);
-    void removeClient(String userId);
-}
