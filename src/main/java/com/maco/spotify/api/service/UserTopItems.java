2ab3af22566eadd65ea29b0e43a7090c2f464d20
razvanmacovei15 <razvanmc15@gmail.com>
2025-04-26 07:44:43 +0300

    sa vedem de merge versiunea 2.0

--- src/main/java/com/maco/spotify/api/service/UserTopItems.java
+++
@@ -1,9 +0,0 @@
-package com.maco.spotify.api.service;
-
-import com.maco.spotify.api.enums.TimeRange;
-
-import java.util.List;
-
-public interface UserTopItems<T> {
-    List<T> getTopItems(TimeRange tr, int limit, int offset);
-}
